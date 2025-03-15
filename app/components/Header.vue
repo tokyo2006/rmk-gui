477f77300e30074ecfe9f1119c5aee06e9cbebe9
liyang <272891021@qq.com>
2025-03-15 15:52:18 +0800

    refactor(components): update Header component to use Select component

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 const hidDevicesStore = useHidDevicesStore();
 const selectedDevicePath = ref();
-watch(selectedDevicePath, async (newValue) => {
-  await invoke('connect_vial_device', { path: newValue });
+watch(selectedDevicePath, async (newValue: string) => {
+  await invoke('connect_vial_device', { path: eval(newValue) });
   await invoke('update_keymap');
 });
 if (hidDevicesStore.devices.length > 0) {
-  selectedDevicePath.value = hidDevicesStore.devices[0]?.path;
+  selectedDevicePath.value = hidDevicesStore.devices[0]?.path.toString();
 } else {
   selectedDevicePath.value = 'Device not found';
 }
@@ -14,10 +14,15 @@ if (hidDevicesStore.devices.length > 0) {
 
 <template>
   <div class="flex justify-start items-center bg-base-100 h-16 border-b border-base-300">
-    <select class="focus:outline-none ml-2" v-model="selectedDevicePath">
-      <option v-for="device in hidDevicesStore.devices" :key="device.product_string" :value="device.path">
-        {{ device.product_string }}
-      </option>
-    </select>
+    <Select v-model="selectedDevicePath">
+      <SelectTrigger class="w-64 ml-2">
+        <SelectValue placeholder="Select a Keyboard"/>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem v-for="device in hidDevicesStore.devices" :key="device.product_string" :value="device.path.toString()"> {{device.product_string}} </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   </div>
 </template>
