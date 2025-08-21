289407298a050375c1015f7cced57e42c369dd2f
LiYang <63987506+liyang8246@users.noreply.github.com>
2025-08-22 03:31:38 +0800

    feat(ConnHID): implement toggle connection (#58)

--- app/components/ConnHID/Web.vue
+++ app/components/ConnHID/Web.vue
@@ -2,14 +2,20 @@
 const keyboardStore = useKeyboardStore()
 const router = useRouter()
 
-const { isLoading: isConnecting, execute: connect } = useAsyncState(
+const { isLoading: isConnecting, execute: toggleConnection } = useAsyncState(
   async () => {
     router.push('/')
-    const device = (await keyboardStore.list()) as HIDDevice[]
-    if (!device[0])
-      return
-    await keyboardStore.connect(device[0])
-    await keyboardStore.fetchAll()
+    if (keyboardStore.isConnected) {
+      await keyboardStore.disconnect()
+      keyboardStore.cleanAll()
+    }
+    else {
+      const device = (await keyboardStore.list()) as HIDDevice[]
+      if (!device[0])
+        return
+      await keyboardStore.connect(device[0])
+      await keyboardStore.fetchAll()
+    }
   },
   undefined,
   { immediate: false },
@@ -30,7 +36,7 @@ const displayName = computed(() => keyboardStore.productName ?? keyboardStore.vi
         :severity="keyboardStore.isConnected ? 'secondary' : 'primary'"
         class="size-full !p-0"
         :loading="isConnecting"
-        @click="connect()"
+        @click="toggleConnection()"
       >
         <Icon
           :name="isConnecting ? 'line-md:loading-twotone-loop' : 'tabler:plug'"
